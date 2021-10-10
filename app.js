9f2d5eee104a0d479407b584d35a82920e140798
singhsduos <ns9628491678@gmail.com>
2021-10-10 11:21:41 +0530

    all files completed

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
-const searchform = document.querySelector('#searchform')
+// Get HTML elements from HTML Doc
+const jokes = document.querySelector('ul');
+const button = document.querySelector('#btn');
+const reset = document.querySelector('#remove');
 
+// function to add new jokes by creating new element in HTML doc
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    var newLI = document.createElement('li');
 
-searchform.addEventListener('submit', async function (e) {
-    e.preventDefault();
-    const searchTerm = searchform.elements.query.value;
-    const config = { params: { q: searchTerm } };
-    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
-    makeImage(res.data);
-    searchform.elements.query.value = "";
+    if (jokeText !== undefined) {
+        newLI.append(jokeText);
+        jokes.append(newLI);
+    } 
+}
 
-});
-
-const makeImage = (shows) => {
-    for (let result of shows) {
-        if (result.show.image) {
-            const img = document.createElement('img');
-            img.src = result.show.image.medium;
-            document.body.append(img);
+// fetching API DadJokes
+const getDadJoke = async () => {
+    try {
+        const config = {
+            headers: {
+                Accept: 'application/json'
+            }
         }
+
+        const res = await axios.get('https://icanhazdadjoke.com/', config);
+        return res.data.joke;
+
+    } catch (e) {
+        return alert("No Jokes Availabale");
     }
 }
+
+// function to remove jokes
+const clearJokes = async () => {
+    try {
+        var newJ = document.querySelector('li');
+        newJ.remove();
+    } catch (e) {
+        return alert("Nothing to remove");
+    }
+}
+
+//buttons for getting jokes and removing jokes
+button.addEventListener('click', addNewJoke);
+reset.addEventListener('click', clearJokes);
